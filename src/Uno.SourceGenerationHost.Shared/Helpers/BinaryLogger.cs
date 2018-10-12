7e7e6bd6cc450a699bf72b9e0ec0ef7cf7883cce
Jérôme Laban <jerome.laban@nventive.com>
2018-10-11 21:57:12 -0400

    Add support for a out-of-process generation host
    This adds better support for macOS and adds support for .NET Core builds.

---
+++ src/Uno.SourceGenerationHost.Shared/Helpers/BinaryLogger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Extensions.Logging;
+using Uno.SourceGeneratorTasks.Helpers;
+
+namespace Uno.SourceGeneration.Helpers
+{
+	internal class BinaryLoggerForwarder : Microsoft.Extensions.Logging.ILogger
+	{
+		private readonly BinaryLoggerEventSource _eventSource;
+		private readonly string _categoryName;
+
+		public BinaryLoggerForwarder(string categoryName, BinaryLoggerEventSource eventSource)
+		{
+			_eventSource = eventSource;
+			_categoryName = categoryName;
+		}
+
+		public IDisposable BeginScope<TState>(TState state) => new DisposableAction(() => { });
+
+		public bool IsEnabled(LogLevel logLevel) => logLevel >= LogLevel.Debug;
+
+		public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+		{
+			if (formatter == null)
+			{
+				throw new ArgumentNullException(nameof(formatter));
+			}
+
+			var message = formatter(state, exception);
+
+			if (string.IsNullOrEmpty(message))
+			{
+				return;
+			}
+
+			switch (logLevel)
+			{
+				case LogLevel.Error:
+					_eventSource.RaiseError(_categoryName, message);
+					break;
+
+				case LogLevel.Warning:
+					_eventSource.RaiseWarning(_categoryName, message);
+					break;
+
+				case LogLevel.Information:
+					_eventSource.RaiseMessage(_categoryName, message, MessageImportance.Normal);
+					break;
+
+				case LogLevel.Debug:
+					_eventSource.RaiseMessage(_categoryName, message, MessageImportance.Low);
+					break;
+
+				default:
+					_eventSource.RaiseMessage(_categoryName, message, MessageImportance.Low);
+					break;
+			}
+		}
+
+		public void Dispose() => throw new NotImplementedException();
+
+	}
+}
