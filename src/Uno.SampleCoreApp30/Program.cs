7107eeae3af476002c150ef0092a6a6e7fda866e
Jérôme Laban <jerome.laban@nventive.com>
2019-10-12 10:16:05 -0400

    Adjust dependency, netcore 2.0 and netcore 3.0 tests

---
+++ src/Uno.SampleCoreApp30/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Uno.SampleCoreApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Debug.WriteLine(Test.MyGeneratedType2.Project);
+		}
+	} 
+}
