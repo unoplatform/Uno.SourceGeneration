48aae1c5ad3d519aa3180ee9e2d149553d9f364a
Youssef Victor <youssefvictor00@gmail.com>
2022-09-09 21:19:46 +0200

    chore: Address review comment

--- src/Uno.SourceGeneration.Engine.Shared/Workspace/ProjectFileInfo.cs
+++ src/Uno.SourceGeneration.Engine.Shared/Workspace/ProjectFileInfo.cs
@@ -46,18 +46,20 @@ namespace Uno.SourceGeneration.Engine.Workspace
         /// </summary>
         public string OutputRefFilePath { get; }
 
-        // /// <summary>
-        // /// The default namespace of the project ("" if not defined, which means global namespace),
-        // /// or null if it is unknown or not applicable. 
-        // /// </summary>
-        // /// <remarks>
-        // /// Right now VB doesn't have the concept of "default namespace". But we conjure one in workspace 
-        // /// by assigning the value of the project's root namespace to it. So various feature can choose to 
-        // /// use it for their own purpose.
-        // /// In the future, we might consider officially exposing "default namespace" for VB project 
-        // /// (e.g. through a "defaultnamespace" msbuild property)
-        // /// </remarks>
-        // public string DefaultNamespace { get; }
+#if false
+        /// <summary>
+        /// The default namespace of the project ("" if not defined, which means global namespace),
+        /// or null if it is unknown or not applicable. 
+        /// </summary>
+        /// <remarks>
+        /// Right now VB doesn't have the concept of "default namespace". But we conjure one in workspace 
+        /// by assigning the value of the project's root namespace to it. So various feature can choose to 
+        /// use it for their own purpose.
+        /// In the future, we might consider officially exposing "default namespace" for VB project 
+        /// (e.g. through a "defaultnamespace" msbuild property)
+        /// </remarks>
+        public string DefaultNamespace { get; }
+#endif
 
         /// <summary>
         /// The target framework of this project.
@@ -90,11 +92,13 @@ namespace Uno.SourceGeneration.Engine.Workspace
         /// </summary>
         public ImmutableArray<ProjectFileReference> ProjectReferences { get; }
 
-        // /// <summary>
-        // /// The error message produced when a failure occurred attempting to get the info. 
-        // /// If a failure occurred some or all of the information may be inaccurate or incomplete.
-        // /// </summary>
-        // public DiagnosticLog Log { get; }
+#if false
+        /// <summary>
+        /// The error message produced when a failure occurred attempting to get the info. 
+        /// If a failure occurred some or all of the information may be inaccurate or incomplete.
+        /// </summary>
+        public DiagnosticLog Log { get; }
+#endif
 
         public override string ToString()
             => string.IsNullOrWhiteSpace(TargetFramework)
